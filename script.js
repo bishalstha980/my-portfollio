0fbc76cde47b13472f78e47ba6b15bd90d2055cb
bishalstha980 <bishalstha980@gmail.com>
2026-03-28 22:49:55 +0545

    Add files via upload

--- script.js
+++ script.js
@@ -32,3 +32,20 @@ submitBtn.addEventListener('click', function (event) {
         alert("Please fill the form!");
     }
 });
+const toggleBtn = document.getElementById('theme-toggle');
+const currentTheme = localStorage.getItem('theme');
+if(currentTheme){
+    document.documentElement.setAttribute('data-theme', currentTheme);
+}
+toggleBtn.addEventListener('click', () => {
+    let theme = document.documentElement.getAttribute('data-theme');
+    if (theme === 'dark'){
+        document.documentElement.setAttribute('data-theme', 'light');
+        localStorage.setItem('theme', 'light');
+        toggleBtn.textcontent = "Dark Mode"
+    } else{
+        document.documentElement.setAttribute('data-theme', 'dark');
+        localStorage.setItem('theme', 'dark');
+        toggleBtn.textContent = "Light Mode";
+    }
+})
